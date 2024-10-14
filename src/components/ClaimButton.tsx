5b972f8b5a4450d99a1cd7610a8c1dd83fdfdb45
vidvidvid <weetopol@gmail.com>
2024-10-14 23:33:52 +0900

    set up shad cn & button

--- src/components/ClaimButton.tsx
+++ src/components/ClaimButton.tsx
@@ -1,18 +1,16 @@
 'use client';
 
+import { Button } from "@/components/ui/button";
+
 const ClaimButton = () => {
   const handleClick = () => {
     console.log('claim');
   };
 
   return (
-    <button
-      type="button"
-      onClick={handleClick}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    >
+    <Button onClick={handleClick} variant="default">
       Claim
-    </button>
+    </Button>
   );
 };
 
