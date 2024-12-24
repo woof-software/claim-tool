c78735574813893bcfcf64f9c8198483400f883e
jipstavenuiter <jipstavenuiter@gmail.com>
2024-12-24 16:28:02 -0300

    feat: remove old sign to login modal

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -2,36 +2,17 @@
 
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import { SignMessage } from '@/components/auth/SignMessage';
-import { SpinningLoader } from '@/components/common/SpinningLoader';
-import { useSession } from '@/hooks/useAuth';
-import { useAccount } from 'wagmi';
 
 export default function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { isConnecting, isConnected } = useAccount();
-  const { data: session, isLoading: isSessionLoading } = useSession();
-
-  const showSignaturePrompt = isConnected && !isSessionLoading && !session;
-
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-grow py-20 px-3">
-        <div className="mx-auto max-w-5xl">
-          {isConnecting || isSessionLoading ? (
-            <div className="flex justify-center items-center h-[calc(100vh-5rem)]">
-              <SpinningLoader />
-            </div>
-          ) : showSignaturePrompt ? (
-            <SignMessage />
-          ) : (
-            children
-          )}
-        </div>
+        <div className="mx-auto max-w-5xl">{children}</div>
       </main>
       <Footer />
     </div>
