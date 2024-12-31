76d1293c5471be085ab7f7c15e8947f5b239020c
jipstavenuiter <jipstavenuiter@gmail.com>
2024-12-31 12:37:37 -0300

    fix: add vid as admin

--- config/storage.ts
+++ config/storage.ts
@@ -3,4 +3,6 @@ export const requiredCsvColumns = ['UUID', 'Title'];
 export const whitelistedAddresses = [
   '0x59266D85D94666D037C1e32dAa8FaC9E95CdaFEf',
   '0x297884BE1c4A418BA4563A0165d85b35C9B7260e',
+  // VID
+  '0x92084Bed8635d82DB600E100539A61a6d4209403',
 ] as `0x${string}`[];
