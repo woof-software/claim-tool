128c009273bef37ca57599be95da96421d4cbf48
jipstavenuiter <jipstavenuiter@gmail.com>
2025-01-03 12:37:49 -0300

    chore: add new admin addresses

--- config/storage.ts
+++ config/storage.ts
@@ -3,6 +3,9 @@ export const requiredCsvColumns = ['UUID', 'Title'];
 export const whitelistedAddresses = [
   '0x59266D85D94666D037C1e32dAa8FaC9E95CdaFEf',
   '0x297884BE1c4A418BA4563A0165d85b35C9B7260e',
-  // VID
+  // V
   '0x92084Bed8635d82DB600E100539A61a6d4209403',
+  // M
+  '0x31f87F1475F81CcfFf6a33A064cAe812BaEEBCC6',
+  '0x636DA9bF416B662B5Fedaf67d5937d07A34c6a2D',
 ] as `0x${string}`[];
