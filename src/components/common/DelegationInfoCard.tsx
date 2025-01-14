693202d7e0239dde4e5b1e843e25caad995b0baa
jipstavenuiter <jipstavenuiter@gmail.com>
2025-01-14 15:07:24 -0300

    feat: layout fixes

--- src/components/common/DelegationInfoCard.tsx
+++ src/components/common/DelegationInfoCard.tsx
@@ -2,15 +2,22 @@ import { Card, CardContent } from '../ui/card';
 
 export default function DelegationInfoCard() {
   return (
-    <Card className="w-[430px] h-[282px] bg-neutral-100 border-none shadow-none">
+    <Card className="w-full  bg-neutral-100 border-none shadow-none">
       <CardContent className="p-10">
         <h2 className="text-xl font-bold mb-6">Why delegate?</h2>
         <p className="text-sm">
           The OP Claim tool is built with{' '}
-          <span className="font-semibold">Hedgey</span>. To allow project
-          builders to claim their tokens, we use a delegation contract.
-          Additionally, all project builders can choose to delegate themselves
-          in this process.
+          <a
+            className="font-semibold"
+            href="https://hedgey.finance/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hedgey
+          </a>
+          . To allow project builders to claim their tokens, we use a delegation
+          contract. Additionally, all project builders can choose to delegate
+          themselves in this process.
         </p>
       </CardContent>
     </Card>
--- src/components/common/ProjectCard.tsx
+++ src/components/common/ProjectCard.tsx
@@ -3,7 +3,7 @@ import type { Grant } from '@/context/GrantsContext';
 
 const ProjectCard = ({ grant }: { grant: Grant }) => {
   return (
-    <Card className="w-[424px] border-black shadow-none bg-transparent">
+    <Card className="w-full border-black shadow-none bg-transparent">
       <CardContent className="py-8 px-10 space-y-6">
         <p>Your project</p>
         <div className="flex items-center">
