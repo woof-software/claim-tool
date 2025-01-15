a9e0185d36178ae2735b853049def4f5ce5771b0
jipstavenuiter <jipstavenuiter@gmail.com>
2025-01-15 14:47:36 -0300

    fix: update contract address for optimism mainnet

--- config/contracts/addresses.ts
+++ config/contracts/addresses.ts
@@ -7,7 +7,7 @@ import {
 } from 'wagmi/chains';
 
 export const hedgeyContractAddresses = {
-  [optimism.id]: '0x5Ae97e4770b7034C7Ca99Ab7edC26a18a23CB412',
+  [optimism.id]: '0x8A2725a6f04816A5274dDD9FEaDd3bd0C253C1A6',
   [mainnet.id]: '0x8A2725a6f04816A5274dDD9FEaDd3bd0C253C1A6',
   [sepolia.id]: '0x8A2725a6f04816A5274dDD9FEaDd3bd0C253C1A6',
   // TODO: Add correct contract addresses for zkSync once they have been deployed
