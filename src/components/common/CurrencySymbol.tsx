1a01187021cc3333a2e4890b6d1c00ff5c6f93fc
jipstavenuiter <jipstavenuiter@gmail.com>
2025-01-20 16:21:50 -0300

    feat: add zksync logo

--- src/components/common/CurrencySymbol.tsx
+++ src/components/common/CurrencySymbol.tsx
@@ -2,21 +2,26 @@ import { CircleHelp } from 'lucide-react';
 import Image from 'next/image';
 
 import type { HedgeyCampaign } from '@/hooks/useGetHedgeyCampaigns';
+import { cn } from '@/lib/utils';
 import ETH from '../../../public/currency_symbols/eth.png';
 import OP from '../../../public/currency_symbols/op.png';
 import POA from '../../../public/currency_symbols/poa.png';
+import ZK from '../../../public/currency_symbols/zk_sync.png';
 
 const getSrc = (currencyName?: string | null) => {
+  console.log('currencyName', currencyName);
   switch (currencyName) {
     case 'ETH':
-      return ETH;
+      return { src: ETH };
     case 'POA':
     case 'PLBR':
-      return POA;
+      return { src: POA };
     case 'OP':
-      return OP;
+      return { src: OP };
+    case 'ZK':
+      return { src: ZK, className: 'bg-white border border-black' };
     default:
-      return undefined;
+      return null;
   }
 };
 
@@ -27,15 +32,17 @@ export const CurrencySymbol = ({
     return <CircleHelp />;
   }
 
-  const src = getSrc(token?.ticker);
+  const result = getSrc(token?.ticker);
 
-  if (!src) {
+  if (!result) {
     return <CircleHelp />;
   }
 
+  const { src, className } = result;
+
   return (
     <Image
-      className="rounded-full flex-shrink-0 flex relative"
+      className={cn('rounded-full flex-shrink-0 flex relative', className)}
       alt={`${token?.name} Logo`}
       src={src}
       width={24}
