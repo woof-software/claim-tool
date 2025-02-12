041c7b078a9b51a67d153ac686883cbf632efc20
gnomadic <edflem+github@gmail.com>
2025-02-12 15:31:18 -0500

    removing agora

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const AGORA_API_URL = process.env.AGORA_API_URL;
-const AGORA_API_KEY = process.env.AGORA_API_KEY;
 const RAILWAY_ENVIRONMENT_NAME = process.env.RAILWAY_ENVIRONMENT_NAME;
 const S3_STORAGE_ENDPOINT = process.env.S3_STORAGE_ENDPOINT;
 const S3_STORAGE_PORT = process.env.S3_STORAGE_PORT;
@@ -10,8 +8,6 @@ const S3_STORAGE_BUCKET_NAME = process.env.S3_STORAGE_BUCKET_NAME;
 
 const nextConfig = {
   env: {
-    AGORA_API_URL: AGORA_API_URL,
-    AGORA_API_KEY: AGORA_API_KEY,
     RAILWAY_ENVIRONMENT_NAME,
     S3_STORAGE_ENDPOINT,
     S3_STORAGE_PORT,
@@ -19,14 +15,7 @@ const nextConfig = {
     S3_STORAGE_SECRET_KEY,
     S3_STORAGE_BUCKET_NAME,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/agora/:path*',
-        destination: `${AGORA_API_URL}/:path*`,
-      },
-    ];
-  },
+
   reactStrictMode: true,
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
