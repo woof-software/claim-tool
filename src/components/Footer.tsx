0963be492f97d88d00c3655a6d42aa68b202387f
Stanislav Baraniuk <stas.b@woof.software>
2025-08-26 17:50:16 +0200

    fix: remove gitcoin logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,6 @@
 
 import { RiGithubFill } from '@remixicon/react';
 import Link from 'next/link';
-import Gitcoin from './common/images/Gitcoin';
 import RaidGuild from './common/images/RaidGuild';
 
 const Footer = () => {
@@ -10,9 +9,6 @@ const Footer = () => {
     <div className="h-24 border-t border-neutral-200 px-3">
       <div className="h-full mx-auto max-w-5xl flex items-center justify-between">
         <div className="flex items-center gap-7">
-          <Link href="https://gitcoin.co/" target="_blank">
-            <Gitcoin />
-          </Link>
           <Link href="https://www.raidguild.org/" target="_blank">
             <RaidGuild />
           </Link>
--- src/components/common/images/Gitcoin.tsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image';
-import GitcoinLogo from '../../../../public/gitcoin.svg';
-const Gitcoin = () => {
-  return <Image src={GitcoinLogo} alt="Gitcoin Logo" />;
-};
-
-export default Gitcoin;
